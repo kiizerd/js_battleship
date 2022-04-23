29bbae8dc7e9d744097514bde52712ef6634142d
kiizerd <ajsewell13@gmail.com>
2022-04-23 16:04:25 -0500

    Added JS entry point.

---
+++ src/js/index.js
@@ -0,0 +1,21 @@
+import PubSub from 'pubsub-js';
+import '../styles/main.scss';
+import Game from './game';
+
+let game = Game();
+game.start();
+
+const resetButtonListener = () => {
+  PubSub.clearAllSubscriptions();
+  game = null;
+  PubSub.clearAllSubscriptions();
+  PubSub.subscribe('RESET_BTN_CLICKED', resetButtonListener);
+  const boards = document.querySelector('#boards');
+  while (boards.firstChild) {
+    boards.removeChild(boards.firstChild);
+  }
+  game = Game();
+  game.start();
+};
+
+PubSub.subscribe('RESET_BTN_CLICKED', resetButtonListener);
